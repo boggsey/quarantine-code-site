98db6afd84b291a747c551c3315467ffc57b2013
boggsey <eric@mlemedia.com>
2020-03-22 22:09:12 -0400

    Resolving issues with padding

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,20 +3,24 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 
 const DetailWrapper = styled.section`
-  ${tw`w-full flex justify-center md:justify-end flex-row bg-black`};
+  ${tw`w-full flex justify-center flex-row bg-black`};
   overflow: hidden;
 
   .detail-container {
-    ${tw`w-5/6 md:w-4/5 flex justify-between flex-col md:flex-row py-8`};
+    ${tw`w-5/6 md:w-full flex justify-between flex-col md:flex-row py-8`};
   }
 
   h2 {
-    ${tw`text-center md:text-left uppercase font-sans font-bold mb-0 mt-0 text-3xl`};
+    ${tw`text-left uppercase font-sans font-bold mb-0 mt-0 text-3xl`};
     color: #9bd675;
   }
 
   .full-description { 
-    ${tw`w-full md:w-1/2 flex flex-col items-start justify-center text-lg leading-normal font-sans text-white text-md`};
+    ${tw`w-full md:w-1/2 flex flex-col items-end justify-center text-lg leading-normal font-sans text-white text-md`};
+  }
+
+  .internal-description {
+    ${tw`w-full md:w-2/3 flex justify-center flex-col`};
   }
 
   .description-container {
@@ -79,38 +83,40 @@ class Details extends Component {
       <DetailWrapper id="details">
         <div className="detail-container">
           <div className="full-description">
-            <h2>
-              Registration
-            </h2>
-            <div className="description-container">
-              <p>Classes will happen via a Google Hangout. It's free but you do have to register. Currently only accepting up to 15 students.</p>
-            </div>
-            <div className="form-container">
-              <form name="contact" method="POST" data-netlify="true">
-                <p>
-                  <label>
-                    <input type="text" name="name" placeholder="Your Name" />
-                  </label>   
-                </p>
-                <p>
-                  <label>
-                    <input type="email" name="email" placeholder="Your Email" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Can you attend all sessions?
-                    {' '}
-                    <select name="sessions[]">
-                      <option value="yes">Yes</option>
-                      <option value="no">No</option>
-                    </select>
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Register</button>
-                </p>
-              </form>
+            <div className="internal-description">
+              <h2>
+                Registration
+              </h2>
+              <div className="description-container">
+                <p>Classes will happen via a Google Hangout. It's free but you do have to register. Currently only accepting up to 15 students.</p>
+              </div>
+              <div className="form-container">
+                <form name="contact" method="POST" data-netlify="true">
+                  <p>
+                    <label>
+                      <input type="text" name="name" placeholder="Your Name" />
+                    </label>   
+                  </p>
+                  <p>
+                    <label>
+                      <input type="email" name="email" placeholder="Your Email" />
+                    </label>
+                  </p>
+                  <p>
+                    <label>
+                      Can you attend all sessions?
+                      {' '}
+                      <select name="sessions[]">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                      </select>
+                    </label>
+                  </p>
+                  <p>
+                    <button type="submit">Register</button>
+                  </p>
+                </form>
+              </div>
             </div>
           
           </div>
